9a8f52bfe147c9ad47e59a32bbf9469c565efadc
minato <mrgastrodon@gmail.com>
2023-01-25 19:14:24 +0900

    add floor_sqrt

---
+++ math/floor_sqrt.hpp
@@ -0,0 +1,7 @@
+/**
+ * sqrt(x) を超えない最大の整数を求める。
+ * O(1)
+ */
+long long floor_sqrt(long long x) {
+    return floor(sqrt((long double)x));
+}
---
+++ test/math/floor_sqrt.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "math/floor_sqrt.hpp";
+#include "other/template.hpp";
+
+int main() {
+    int T;
+    cin >> T;
+    rep(i, T) {
+        ll N;
+        cin >> N;
+        for (ll x = 2;; x++) {
+            if (N % x == 0) {
+                ll y = N / x;
+                ll z = floor_sqrt(y);
+                if (z * z == y) {
+                    cout << z << " " << x << ln;
+                } else {
+                    y /= x;
+                    cout << x << " " << y << ln;
+                }
+                break;
+            }
+        }
+    }
+}
