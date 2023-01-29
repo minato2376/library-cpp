dbf1449e7b13fc621efdf48777407b0577f9afe2
minato <mrgastrodon@gmail.com>
2023-01-30 01:32:32 +0900

    add warshall_floyd

---
+++ shortestpath/warshall_floyd.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+template <typename Int, Int INF> void warshall_floyd(vector<vector<Int>>& G) {
+    int N = G.size();
+
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            if (G[i][k] == INF) continue;
+            for (int j = 0; j < N; j++) {
+                if (G[k][j] == INF) continue;
+                G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
+            }
+        }
+    }
+    return;
+}
